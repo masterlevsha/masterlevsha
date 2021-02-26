0c856105450b8b33cefbbd19c9d25134c9a4e4ce
masterlevsha <79583858+masterlevsha@users.noreply.github.com>
2021-02-26 17:09:43 +0300

    Update settings.py

--- tehnika/settings.py
+++ tehnika/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['levsha.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
